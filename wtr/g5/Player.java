301609d60c13f38e3ccec2825cc0705a05392037
jg3527 <jg3527@columbia.edu>
2015-11-24 12:56:08 -0500

    fixed bug

--- wtr/g5/Player.java
+++ wtr/g5/Player.java
@@ -25,6 +25,7 @@ public class Player implements wtr.sim.Player {
 	private int interfereCount = 0;
 	private Integer preChatId;
 	private Point selfPlayer;
+	private int preTryId;
 	// init function called once
 	public void init(int id, int[] friend_ids, int strangers)
 	{
@@ -142,14 +143,14 @@ public class Player implements wtr.sim.Player {
 			double dx = self.x - p.x;
 			double dy = self.y - p.y;
 			double dd = dx * dx + dy * dy;
-			if (dd <= 4.0 && dd < minDis){
+			if (dd>= 0.25 && dd <= 4.0 && dd < minDis){
 				find = true;
 				targetId = p.id;
 				minDis = dd;
 			}
 		}
 		//TODO make sure it's not in blacklist as well
-		if(find && minDis >= 0.25 && isAlone(targetId, players, chat_ids) && W[targetId] != 0){
+		if(find && isAlone(targetId, players, chat_ids) && W[targetId] != 0){
 			preChatId = targetId;
 			return new Point(0.0, 0.0, targetId);
 		}
@@ -184,7 +185,7 @@ public class Player implements wtr.sim.Player {
 
 	public Point getCloser(Point self, Point target){
 		//can't set to 0.5, if 0.5 the result distance may be 0.49
-		double targetDis = 0.6;
+		double targetDis = 0.5 + (2 - 0.5) * random.nextDouble();
 		double dis = distance(self, target);
 		double x = (dis - targetDis) * (target.x - self.x) / dis;
 		double y = (dis - targetDis) * (target.y - self.y) / dis;
